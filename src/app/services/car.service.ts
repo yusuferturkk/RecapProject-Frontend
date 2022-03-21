0906ad0a3b3f8a3f52e0c309b4e134e100552552
Yusuf Ertürk <yusuf_erturk98@hotmail.com>
2022-03-21 03:37:27 +0300

    18. Gün - Ödev

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -8,10 +8,15 @@ import { ListResponseModel } from '../models/listResponseModel';
   providedIn: 'root'
 })
 export class CarService {
-  apiUrl = 'https://localhost:44371/api/cars/getall';
+  apiUrl = 'https://localhost:44371/api/';
   constructor(private httpClient: HttpClient) { }
 
   getCars(): Observable<ListResponseModel<Car>>{
     return this.httpClient.get<ListResponseModel<Car>>(this.apiUrl);
+  }
+
+  getCarsByBrand(brandId:number): Observable<ListResponseModel<Car>>{
+    let newPath = this.apiUrl + "cars/getcarsbybrandid?brandId" + brandId
+    return this.httpClient.get<ListResponseModel<Car>>(newPath);
   }
 }
